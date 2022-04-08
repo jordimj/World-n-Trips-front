d1ae19243bb16776e8a30b01deaa1cca1ef88573
jordimj <jurado.jordi@gmail.com>
2022-04-08 17:58:10 +0200

    Fix active classname after React Router v6 upgrade

--- src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -3,7 +3,11 @@ import styles from './NavigationItem.module.css';
 
 const navigationItem = ({ exact, link, children }) => (
   <li className={styles.navigationItem}>
-    <NavLink exact={exact} to={link} activeClassName={styles.active}>
+    <NavLink
+      exact={exact}
+      to={link}
+      className={({ isActive }) => isActive && styles.active}
+    >
       {children}
     </NavLink>
   </li>
