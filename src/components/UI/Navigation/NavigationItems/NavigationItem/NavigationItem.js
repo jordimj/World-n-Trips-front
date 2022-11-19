bff6ec12f65b38df0f2e5f693520dc54b2fda63b
jordimj <jurado.jordi@gmail.com>
2022-11-19 17:35:38 +0100

    Remove unneeded exact prop in NavLink after upgrading React Router

--- src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,13 +1,9 @@
 import { NavLink } from 'react-router-dom';
 import styles from './NavigationItem.module.css';
 
-const navigationItem = ({ exact, link, children }) => (
+const navigationItem = ({ link, children }) => (
   <li className={styles.navigationItem}>
-    <NavLink
-      exact={exact}
-      to={link}
-      className={({ isActive }) => (isActive ? styles.active : undefined)}
-    >
+    <NavLink to={link} className={({ isActive }) => (isActive ? styles.active : '')}>
       {children}
     </NavLink>
   </li>
--- src/components/UI/Navigation/NavigationItems/NavigationItems.js
+++ src/components/UI/Navigation/NavigationItems/NavigationItems.js
@@ -3,9 +3,7 @@ import styles from './NavigationItems.module.css';
 
 const navigationItems = () => (
   <ul className={styles.navigationItems}>
-    <NavigationItem link="/" exact>
-      World map
-    </NavigationItem>
+    <NavigationItem link="/">World map</NavigationItem>
     <NavigationItem link="/countriesList">List of countries</NavigationItem>
     <NavigationItem link="/statistics">Travel stats</NavigationItem>
     <NavigationItem link="/trips">Trip journals</NavigationItem>
