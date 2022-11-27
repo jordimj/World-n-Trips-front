4bfd20cd15978fd728c70ddc80864be0531ab683
jordimj <jurado.jordi@gmail.com>
2022-11-27 10:43:35 +0100

    Add code-splitting to reduce bundle size

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -1,23 +1,28 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from '../components/Home/Home';
-import CountriesList from '../components/CountriesList/CountriesList';
-import Country from '../components/Country/Country';
-import TravelStats from '../components/TravelStats/TravelStats';
-import Trips from '../components/Trips/Trips';
-import Journals from '../components/Journals/Journals';
 import Layout from '../components/UI/Layout/Layout';
+import Spinner from '../components/UI/Spinner/Spinner';
+
+const Home = lazy(() => import('../components/Home/Home'));
+const CountriesList = lazy(() => import('../components/CountriesList/CountriesList'));
+const Country = lazy(() => import('../components/Country/Country'));
+const TravelStats = lazy(() => import('../components/TravelStats/TravelStats'));
+const Trips = lazy(() => import('../components/Trips/Trips'));
+const Journals = lazy(() => import('../components/Journals/Journals'));
 
 export const AppRouter = () => (
-  <BrowserRouter>
-    <Layout>
-      <Routes>
-        <Route path="/country/:countryCode" element={<Country />} />
-        <Route path="/countriesList" element={<CountriesList />} />
-        <Route path="/statistics" element={<TravelStats />} />
-        <Route path="/trips" element={<Trips />} />
-        <Route path="/journals/:tripId" element={<Journals />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Layout>
-  </BrowserRouter>
+  <Suspense fallback={<Spinner />}>
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/country/:countryCode" element={<Country />} />
+          <Route path="/countriesList" element={<CountriesList />} />
+          <Route path="/statistics" element={<TravelStats />} />
+          <Route path="/trips" element={<Trips />} />
+          <Route path="/journals/:tripId" element={<Journals />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  </Suspense>
 );
