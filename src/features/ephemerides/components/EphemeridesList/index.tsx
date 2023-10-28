7e6d8e36053608b056f42ac9d5e4d108fe97961c
jordimj <jurado.jordi@gmail.com>
2023-10-29 00:36:19 +0200

    feat(ephemeris): add text when no ephemeris

--- src/features/ephemerides/components/EphemeridesList/index.tsx
+++ src/features/ephemerides/components/EphemeridesList/index.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Stack } from '@mui/material';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Typography from '@mui/material/Typography';
-import { Stack } from '@mui/material';
 import CountryFlag from '@/template/components/CountryFlag';
-import { formatFullDate, getYearsAgo } from '@/utils/date';
 import { buildTripName } from '@/utils';
+import { formatFullDate, getYearsAgo } from '@/utils/date';
 import { Ephemerides } from '../../interfaces';
 
 interface EphemeridesListProps {
@@ -23,40 +23,44 @@ function EphemeridesList(props: EphemeridesListProps) {
   return (
     <Stack gap={2} sx={{ m: 2, mb: 1 }}>
       <Typography sx={{ fontSize: 20 }}>Travel ephemerides of the day</Typography>
-      <List sx={{ p: 0, marginTrim: 'inline-end' }}>
-        {ephemerides.map((ephemeris, idx) => (
-          <ListItem key={idx} sx={{ p: 2 }}>
-            <Stack sx={{ width: '100%' }}>
-              <Typography
-                sx={{ fontSize: 14, fontWeight: 700, ml: 'auto' }}
-                onClick={toggleShowTimeAgo}
-              >
-                {showTimeAgo
-                  ? getYearsAgo(ephemeris.date)
-                  : formatFullDate(ephemeris.date)}
-              </Typography>
-              {ephemeris.country ? (
-                <Stack direction="row" alignItems="center">
-                  <NavLink
-                    to={`/countries/${ephemeris.country.alpha3code}/`}
-                    onClick={onClose}
-                  >
-                    <CountryFlag name={ephemeris.country.name} height={75} />
-                  </NavLink>
-                  <Stack sx={{ ml: 3 }}>
-                    <Typography>
-                      I slept in {ephemeris.city}, {ephemeris.country.name}
-                    </Typography>
-                    <Typography>During the {buildTripName(ephemeris.trip)}</Typography>
+      {ephemerides.length === 0 ? (
+        <Typography fontSize={14}>There are no ephemerides today.</Typography>
+      ) : (
+        <List sx={{ p: 0, marginTrim: 'inline-end' }}>
+          {ephemerides.map((ephemeris, idx) => (
+            <ListItem key={idx} sx={{ p: 2 }}>
+              <Stack sx={{ width: '100%' }}>
+                <Typography
+                  sx={{ fontSize: 14, fontWeight: 700, ml: 'auto' }}
+                  onClick={toggleShowTimeAgo}
+                >
+                  {showTimeAgo
+                    ? getYearsAgo(ephemeris.date)
+                    : formatFullDate(ephemeris.date)}
+                </Typography>
+                {ephemeris.country ? (
+                  <Stack direction="row" alignItems="center">
+                    <NavLink
+                      to={`/countries/${ephemeris.country.alpha3code}/`}
+                      onClick={onClose}
+                    >
+                      <CountryFlag name={ephemeris.country.name} height={75} />
+                    </NavLink>
+                    <Stack sx={{ ml: 3 }}>
+                      <Typography>
+                        I slept in {ephemeris.city}, {ephemeris.country.name}
+                      </Typography>
+                      <Typography>During the {buildTripName(ephemeris.trip)}</Typography>
+                    </Stack>
                   </Stack>
-                </Stack>
-              ) : (
-                <Typography>I was in my {buildTripName(ephemeris.trip)}</Typography>
-              )}
-            </Stack>
-          </ListItem>
-        ))}
-      </List>
+                ) : (
+                  <Typography>I was in my {buildTripName(ephemeris.trip)}</Typography>
+                )}
+              </Stack>
+            </ListItem>
+          ))}
+        </List>
+      )}
     </Stack>
   );
 }
