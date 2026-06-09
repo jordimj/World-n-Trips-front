967eb8ea8adc9229f7d5bb1192800c6c8aae684d
jordimj <jurado.jordi@gmail.com>
2026-06-09 21:32:27 +0200

    chore: :fire: kill unused service worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
-
-registerServiceWorker();
